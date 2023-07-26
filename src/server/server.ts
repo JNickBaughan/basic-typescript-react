297c451feee16bf256a6627499c8d966b33f6e5a
J. Nick Baughan <JNickBaughan@Gmail.com>
2023-07-26 18:44:34 -0400

    WIP - init setup for server

---
+++ src/server/server.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express';
+import bodyParser from "body-parser";
+let PORT = 3000;
+let server: express.Application = express();
+
+const middlewares = [
+  express.static("dist"),
+  bodyParser.json(),
+  bodyParser.urlencoded({ extended: true })
+];
+	
+server.use(...middlewares);
+
+server.get("/", (req: Request, res: Response) => {
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+      <body>
+        <div id="root" />
+        <script src="bundle.js"></script>
+        test
+      </body>
+    </html>
+    `)
+})
+
+server.listen(PORT, function(){
+	console.log("server listening on port: " + PORT);
+});
