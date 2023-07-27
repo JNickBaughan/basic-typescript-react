0c9b44704f5479e6fab40e2c5db46cc774c56a9e
J. Nick Baughan <JNickBaughan@Gmail.com>
2023-07-27 07:04:23 -0400

    wip - add client code

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const common = require("./webpack.common.js")
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 
 module.exports = merge(common, {
-    entry: "./src/index",
+    entry: "./src/browser/app.ts",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
